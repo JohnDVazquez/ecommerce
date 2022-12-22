f53b78678faf2e0b432660e856b2022de1b13441
JohnDVazquez <johndvazquez2000@gmail.com>
2022-12-22 13:26:51 -0600

    Javascript

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let string = "Juan"
+console.log(string)
+let numero = 221321
+console.log(numero)
+let booleano = false
+console.log(booleano)
+let indefinido = undefined
+console.log(indefinido)
+let vacio = null
+console.log(vacio)
+
+let nombre = "Jonathan"
+let edad = 22
+console.log("Hola, soy " + nombre + " y tengo " + edad + " años y estoy practicando concatenacion") 
+console.log(`Hola, soy ${nombre} tengo ${edad} años y estoy practicando interpolacion`)
+
+let x = 58
+let y = 19
+z=x+y
+console.log(`El resultado de la suma es ${z}`)
+
+console.log(x==y)
