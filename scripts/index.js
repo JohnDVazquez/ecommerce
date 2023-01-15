23568a34694124abe6af6325222b6f52ab3c4da8
JohnDVazquez <johndvazquez2000@gmail.com>
2023-01-15 13:39:08 -0600

    Semana 6

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,30 @@
-let string = "Juan"
-console.log(string)
-let numero = 221321
-console.log(numero)
-let booleano = false
-console.log(booleano)
-let indefinido = undefined
-console.log(indefinido)
-let vacio = null
-console.log(vacio)
+//let string = "Juan";
+//console.log(string);
+//let numero = 221321;
+//console.log(numero);
+//let booleano = false;
+//console.log(booleano);
+//let indefinido = undefined;
+//console.log(indefinido);
+//let vacio = null;
+//console.log(vacio);
 
-let nombre = "Jonathan"
-let edad = 22
-console.log("Hola, soy " + nombre + " y tengo " + edad + " años y estoy practicando concatenacion") 
-console.log(`Hola, soy ${nombre} tengo ${edad} años y estoy practicando interpolacion`)
+//let nombre = "Jonathan";
+//let edad = 22;
+//console.log(
+//    "Hola, soy " +
+//    nombre +
+//    " y tengo " +
+//    edad +
+//    " años y estoy practicando concatenacion"
+//);
+//console.log(
+//    `Hola, soy ${nombre} tengo ${edad} años y estoy practicando interpolacion`
+//);
 
-let x = 58
-let y = 19
-z=x+y
-console.log(`El resultado de la suma es ${z}`)
+//let x = 58;
+//let y = 19;
+//z = x + y;
+//console.log(`El resultado de la suma es ${z}`);
 
-console.log(x==y)
+//console.log(x == y);
