6cd90ed77c667d9f9004722acf1b51aca268ffca
JohnDVazquez <johndvazquez2000@gmail.com>
2023-02-08 17:14:00 -0600

    Nueva4

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,29 @@
 //console.log(`El resultado de la suma es ${z}`);
 
 //console.log(x == y);
+const carrito = [];
+
+const agregarAlCarrito = (producto, carrito) => {
+    carrito.push(producto);
+    console.log("Se agrego con exito el producto al carrito!");
+}
+
+const pasteles = [
+    {id:1, nombre:"Pastel con diseño de meme", precio: 350, img:"img.jpg"},
+    {id:2, nombre:"Pastel de chocolate y fresas", precio: 400, img:"img.jpg"},
+    {id:3, nombre:"Pastel de vw", precio: 340, img:"img.jpg"},
+    {id:4, nombre:"Pastel de cafe", precio: 250, img:"img.jpg"},
+    {id:5, nombre:"Pastel de chocolate", precio: 450, img:"img.jpg"},
+    {id:6, nombre:"Pastel de ferrero", precio: 450, img:"img.jpg"},
+    {id:7, nombre:"Pastel de fresa", precio: 420, img:"img.jpg"},
+    {id:8, nombre:"Pastel de galleta oreo", precio: 350, img:"img.jpg"},
+    {id:9, nombre:"Pastel de moka", precio: 250, img:"img.jpg"},
+    {id:10, nombre:"Pastel de patricio", precio: 350, img:"img.jpg"},
+    {id:11, nombre:"Pastel de piñon", precio: 500, img:"img.jpg"},
+    {id:12, nombre:"Pastel estilo campestre", precio: 460, img:"img.jpg"},
+    {id:13, nombre:"Cheescake tradicional", precio: 300, img:"img.jpg"},
+];
+
+for (let i=0; i < pasteles.length; i++){
+    console.log ("Array completo: ", pasteles[i]);
+}
