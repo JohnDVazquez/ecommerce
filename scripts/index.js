1bfeec47a7d183786ccacc0bafe8d920d8565d99
JohnDVazquez <johndvazquez2000@gmail.com>
2023-02-16 14:11:53 -0600

    solving problems

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,19 @@ const contenidoCarrito = document.getElementById("contenidoCarrito");
 const botonEliminar = document.getElementById("botonEliminar");
 
 const pasteles = [
-    {id:1, nombre:"Pastel con diseño de meme", precio: 350, img:"../imagenes/Pastel c meme.png", cantidad: 1},
-    {id:2, nombre:"Pastel de chocolate y fresas", precio: 400, img:"../imagenes/pastel chocolate y fresas.PNG", cantidad: 1},
-    {id:3, nombre:"Pastel con diseño camioneta vw", precio: 340, img:"../imagenes/Pastel con diseño vw.jpeg", cantidad: 1},
-    {id:4, nombre:"Pastel de cafe", precio: 250, img:"../imagenes/Pastel de cafe.png", cantidad: 1},
-    {id:5, nombre:"Pastel de chocolate", precio: 450, img:"../imagenes/pastel de chocolate.jpg", cantidad: 1},
-    {id:6, nombre:"Pastel de ferrero", precio: 450, img:"../imagenes/pastel de ferrero.jpg", cantidad: 1},
-    {id:7, nombre:"Pastel de fresa", precio: 420, img:"../imagenes/pastel de fresa.jpg", cantidad: 1},
-    {id:8, nombre:"Pastel de galleta oreo", precio: 350, img:"../imagenes/pastel de galleta oreo.png", cantidad: 1},
-    {id:9, nombre:"Pastel de moka", precio: 250, img:"../imagenes/Pastel de moka.jpg", cantidad: 1},
-    {id:10, nombre:"Pastel de patricio", precio: 350, img:"../imagenes/Pastel de patricio.jpg", cantidad: 1},
-    {id:11, nombre:"Pastel de piñon", precio: 500, img:"../imagenes/Pastel de piñon.jpg", cantidad: 1},
-    {id:12, nombre:"Pastel estilo campestre", precio: 460, img:"../imagenes/Pastel estilo campestre.jpg", cantidad: 1},
-    {id:13, nombre:"Cheescake tradicional", precio: 300, img:"../imagenes/cheescake.jpeg", cantidad: 1},
+    {id:1, nombre:"Pastel con diseño de meme", precio: 350, img:"./imagenes/Pastel c meme.png", cantidad: 1},
+    {id:2, nombre:"Pastel de chocolate y fresas", precio: 400, img:"./imagenes/pastel chocolate y fresas.PNG", cantidad: 1},
+    {id:3, nombre:"Pastel con diseño camioneta vw", precio: 340, img:"./imagenes/Pastel con diseño vw.jpeg", cantidad: 1},
+    {id:4, nombre:"Pastel de cafe", precio: 250, img:"./imagenes/Pastel de cafe.png", cantidad: 1},
+    {id:5, nombre:"Pastel de chocolate", precio: 450, img:"./imagenes/pastel de chocolate.jpg", cantidad: 1},
+    {id:6, nombre:"Pastel de ferrero", precio: 450, img:"./imagenes/pastel de ferrero.jpg", cantidad: 1},
+    {id:7, nombre:"Pastel de fresa", precio: 420, img:"./imagenes/pastel de fresa.jpg", cantidad: 1},
+    {id:8, nombre:"Pastel de galleta oreo", precio: 350, img:"./imagenes/pastel de galleta oreo.png", cantidad: 1},
+    {id:9, nombre:"Pastel de moka", precio: 250, img:"./imagenes/Pastel de moka.jpg", cantidad: 1},
+    {id:10, nombre:"Pastel de patricio", precio: 350, img:"./imagenes/Pastel de patricio.jpg", cantidad: 1},
+    {id:11, nombre:"Pastel de piñon", precio: 500, img:"./imagenes/Pastel de piñon.jpg", cantidad: 1},
+    {id:12, nombre:"Pastel estilo campestre", precio: 460, img:"./imagenes/Pastel estilo campestre.jpg", cantidad: 1},
+    {id:13, nombre:"Cheescake tradicional", precio: 300, img:"./imagenes/cheescake.jpeg", cantidad: 1},
 ];
 
 const carrito = [];
